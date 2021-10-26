ed98b1a7460b2905869164bcdea27041b658d1f3
thewa <thewa@192.168.1.212>
2021-10-25 23:13:13 -0700

    Added painting

--- BoldAndBrash/src/boldandbrash/BoldAndBrash.java
+++ BoldAndBrash/src/boldandbrash/BoldAndBrash.java
@@ -16,6 +16,9 @@ public class BoldAndBrash {
      */
     public static void main(String[] args) {
         System.out.println("This is just a test");
+        Painting temp = new Painting(24);
+        System.out.println(temp.getDimension());
+        
     }
     
 }
