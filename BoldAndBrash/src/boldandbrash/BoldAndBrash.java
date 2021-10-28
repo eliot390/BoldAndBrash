eb720079e1a07b3020678e050b71ef6f69649bd4
thewa <thewa@192.168.1.212>
2021-10-27 19:39:10 -0700

    Just testing things

--- BoldAndBrash/src/boldandbrash/BoldAndBrash.java
+++ BoldAndBrash/src/boldandbrash/BoldAndBrash.java
@@ -1,19 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package boldandbrash;
 
-/**
- *
- * @author thewa
- */
+
 public class BoldAndBrash {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("This is just a test");
         Painting temp = new Painting(24);
