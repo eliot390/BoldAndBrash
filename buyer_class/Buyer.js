07ef91187606ab71091d903adb451020cc53db00
Smooth Toast <thewaffleminister@gmail.com>
2021-11-06 22:11:59 -0700

    Added the buyer function

--- buyer_class/Buyer.js
+++ buyer_class/Buyer.js
@@ -1,1 +1,11 @@
 console.log("Just need to make sure this works");
+function Buyer(username, address, password) {
+    this.username = username;
+    this.address = address;
+    this.password = password;
+}
+
+let Dave = new Buyer("daveB", "263564 Merrylane Road", "12345");
+console.log(Dave.username);
+console.log(Dave.address);
+console.log(Dave.password);
