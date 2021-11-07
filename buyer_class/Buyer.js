b62bb63deaf8bb7d9fb394052311b1ac0bdf1c22
Smooth Toast <thewaffleminister@gmail.com>
2021-11-07 01:46:30 -0700

    Added painting support

--- buyer_class/Buyer.js
+++ buyer_class/Buyer.js
@@ -16,7 +16,7 @@ function Buyer(fullName, username, address, password) {
         console.log("Password: " + password);
         console.log("Paintings with successful bids: ");
         for(let i = 0; i < bidArrays.length; i++){
-            console.log(bidArrays[i].name);
+            console.log(bidArrays[i].name + " with a bid of " + bidArrays[i].highestBid);
         }
 
     }
@@ -38,13 +38,24 @@ function Buyer(fullName, username, address, password) {
         }
     }
 
+    Buyer.prototype.setBidder = function(bid, painting){
+        for(let i = 0; i < bidArrays.length; i++){
+            if(painting ===bidArrays[i].name && bidArrays[i].price < bid){
+                bidArrays[i].setBidder(bid);
+                console.log("Successfully bid " + bid + " on " + bidArrays[i].name);
+            } else if(painting ===bidArrays[i].name && bidArrays[i].price > bid) {
+                console.log("Sorry, your bid of " + bid + " is under " + bidArrays[i].price);
+            }
+        }
+    }
+
 }
 
 let Dave = new Buyer("Dave Baptist","daveB", "263564 Merrylane Road", "12345");
 console.log(Dave.username);
 console.log(Dave.address);
 console.log(Dave.password);
-let MonaLisa = new Painting("Mona Lisa", 40000, 0);
+let MonaLisa = new Painting("Mona Lisa", 40000, 40000);
 let Scream = new Painting("Scream", 25000, 12000);
 let ObamaPortrait = new Painting("Painting of Obama", 100000, 0);
 let LincolnPortrait = new Painting("Painting of Abraham Lincoln", 20000000, 0);
@@ -54,5 +65,7 @@ Dave.insertBidItem(ObamaPortrait);
 Dave.insertBidItem(LincolnPortrait);
 Dave.printBuyer();
 console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+Dave.setBidder(6900000, "Painting of Obama");
+Dave.setBidder(420, "Mona Lisa");
 Dave.removeBidItem("Scream");
 Dave.printBuyer();
