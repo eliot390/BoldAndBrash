e69763f927c79e5d543d2149e439ef448562ac7b
Smooth Toast <thewaffleminister@gmail.com>
2021-11-06 22:26:17 -0700

    Added printBuyer

--- buyer_class/Buyer.js
+++ buyer_class/Buyer.js
@@ -1,11 +1,22 @@
 console.log("Just need to make sure this works");
-function Buyer(username, address, password) {
+function Buyer(fullName, username, address, password) {
+    this.fullName = fullName;
     this.username = username;
     this.address = address;
     this.password = password;
+    Buyer.prototype.printBuyer = function(){
+        console.log("Full Name: " + fullName);
+        console.log("Username: " + username);
+        console.log("Address: " + address);
+        console.log("Password: " + password);
+    }
 }
 
-let Dave = new Buyer("daveB", "263564 Merrylane Road", "12345");
+let Dave = new Buyer("Dave Baptist","daveB", "263564 Merrylane Road", "12345");
 console.log(Dave.username);
 console.log(Dave.address);
 console.log(Dave.password);
+Dave.printBuyer();
+
+
+
