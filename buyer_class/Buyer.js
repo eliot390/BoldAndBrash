fe4175a5551934126ce1c379a4effa286fe7d3cd
Smooth Toast <thewaffleminister@gmail.com>
2021-11-07 01:27:32 -0700

    Added painting support

--- buyer_class/Buyer.js
+++ buyer_class/Buyer.js
@@ -1,4 +1,5 @@
-console.log("Just need to make sure this works");
+
+const {Painting} = require('./TestPainting.js');
 function Buyer(fullName, username, address, password) {
 
     const bidArrays = [];
@@ -13,7 +14,10 @@ function Buyer(fullName, username, address, password) {
         console.log("Username: " + username);
         console.log("Address: " + address);
         console.log("Password: " + password);
-        console.log("Paintings with successful bids: " + bidArrays)
+        console.log("Paintings with successful bids: ");
+        for(let i = 0; i < bidArrays.length; i++){
+            console.log(bidArrays[i].name);
+        }
 
     }
 
@@ -26,8 +30,8 @@ function Buyer(fullName, username, address, password) {
     Buyer.prototype.removeBidItem = function(painting) {
 
         for(let i = 0; i < bidArrays.length; i++) {
-            if(painting == bidArrays[i]){
-                console.log(bidArrays[i] + " has been removed");
+            if(painting === bidArrays[i].name){
+                console.log(bidArrays[i].name + " has been removed");
                 bidArrays.splice(i,1);
 
             }
@@ -40,11 +44,15 @@ let Dave = new Buyer("Dave Baptist","daveB", "263564 Merrylane Road", "12345");
 console.log(Dave.username);
 console.log(Dave.address);
 console.log(Dave.password);
-Dave.insertBidItem("Mona Lisa");
-Dave.insertBidItem("The Scream");
-Dave.insertBidItem("Octopus");
-Dave.insertBidItem("Obama");
-Dave.insertBidItem("Lincoln");
+let MonaLisa = new Painting("Mona Lisa", 40000, 0);
+let Scream = new Painting("Scream", 25000, 12000);
+let ObamaPortrait = new Painting("Painting of Obama", 100000, 0);
+let LincolnPortrait = new Painting("Painting of Abraham Lincoln", 20000000, 0);
+Dave.insertBidItem(MonaLisa);
+Dave.insertBidItem(Scream);
+Dave.insertBidItem(ObamaPortrait);
+Dave.insertBidItem(LincolnPortrait);
 Dave.printBuyer();
-Dave.removeBidItem("Octopus");
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+Dave.removeBidItem("Scream");
 Dave.printBuyer();
