c912994322f8123e07dbd79ad5d462640d89b217
Smooth Toast <thewaffleminister@gmail.com>
2021-11-07 21:22:14 -0800

    Added support for multiple objects

--- buyer_class/Buyer.js
+++ buyer_class/Buyer.js
@@ -2,48 +2,48 @@
 const {Painting} = require('./TestPainting.js');
 function Buyer(fullName, username, address, password) {
 
-    const bidArrays = [];           //For bids on paintings
-    const orders = [];              //For successful orders
+    this.bidArrays = [];           //For bids on paintings
+    this.orders = [];              //For successful orders
     this.fullName = fullName;       //Full name for buyer
     this.username = username;       //Username for buyer
     this.address = address;         //Address for buyer
     this.password = password;       //Password for buyer
 
     Buyer.prototype.printOrders = function() {
-        for(let i = 0; i < orders.length; i++) {
-            console.log(orders[i].name);
+        for(let i = 0; i < this.orders.length; i++) {
+            console.log(this.orders[i].name);
         }
     }
 
     Buyer.prototype.insertOrders = function(painting) {
-        orders.push(painting);
+        this.orders.push(painting);
     }
 
     Buyer.prototype.printBuyer = function() {
 
-        console.log("Full Name: " + fullName);
-        console.log("Username: " + username);
-        console.log("Address: " + address);
-        console.log("Password: " + password);
+        console.log("Full Name: " + this.fullName);
+        console.log("Username: " + this.username);
+        console.log("Address: " + this.address);
+        console.log("Password: " + this.password);
         console.log("Paintings with successful bids: ");
-        for(let i = 0; i < bidArrays.length; i++){
-            console.log(bidArrays[i].name + " with a bid of " + bidArrays[i].highestBid);
+        for(let i = 0; i < this.bidArrays.length; i++){
+            console.log(this.bidArrays[i].name + " with a bid of " + this.bidArrays[i].highestBid);
         }
-
+        this.printOrders();
     }
 
     Buyer.prototype.insertBidItem = function(painting) {
 
-            bidArrays.push(painting);
+            this.bidArrays.push(painting);
 
     }
 
     Buyer.prototype.removeBidItem = function(painting) {
 
-        for(let i = 0; i < bidArrays.length; i++) {
-            if(painting === bidArrays[i].name){
-                console.log(bidArrays[i].name + " has been removed");
-                bidArrays.splice(i,1);
+        for(let i = 0; i < this.bidArrays.length; i++) {
+            if(painting === this.bidArrays[i].name){
+                console.log(this.bidArrays[i].name + " has been removed");
+                this.bidArrays.splice(i,1);
 
             }
         }
@@ -51,12 +51,12 @@ function Buyer(fullName, username, address, password) {
 
 
     Buyer.prototype.setBidder = function(bid, painting) {
-        for(let i = 0; i < bidArrays.length; i++){
-            if(painting ===bidArrays[i].name && bidArrays[i].price < bid){
-                bidArrays[i].setBidder(bid);
-                console.log("Successfully bid " + bid + " on " + bidArrays[i].name);
-            } else if(painting ===bidArrays[i].name && bidArrays[i].price > bid) {
-                console.log("Sorry, your bid of " + bid + " is under " + bidArrays[i].price);
+        for(let i = 0; i < this.bidArrays.length; i++){
+            if(painting ===this.bidArrays[i].name && this.bidArrays[i].price < bid){
+                this.bidArrays[i].setBidder(bid);
+                console.log("Successfully bid " + bid + " on " + this.bidArrays[i].name);
+            } else if(painting === this.bidArrays[i].name && this.bidArrays[i].price > bid) {
+                console.log("Sorry, your bid of " + bid + " is under " + this.bidArrays[i].price);
             }
         }
     }
@@ -64,6 +64,7 @@ function Buyer(fullName, username, address, password) {
 }
 
 let Dave = new Buyer("Dave Baptist","daveB", "263564 Merrylane Road", "12345");
+let Mike = new Buyer("Mike Lee Torres", "mLee", "12345 Something Road", "iamcool69");
 console.log(Dave.username);
 console.log(Dave.address);
 console.log(Dave.password);
@@ -81,4 +82,10 @@ Dave.setBidder(6900000, "Painting of Obama");
 Dave.setBidder(420, "Mona Lisa");
 Dave.removeBidItem("Scream");
 Dave.insertOrders(Scream);
-Dave.printOrders();
+Dave.printBuyer();
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+Mike.printBuyer();
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+Dave.printBuyer();
+
+Mike.printBuyer();
