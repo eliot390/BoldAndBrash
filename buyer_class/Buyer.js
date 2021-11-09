d7043f0cfb0c2e5fbfda92477bf4c0036e38a2e4
Smooth Toast <thewaffleminister@gmail.com>
2021-11-08 20:19:37 -0800

    Added set/get and updated setBidder

--- buyer_class/Buyer.js
+++ buyer_class/Buyer.js
@@ -1,5 +1,3 @@
-
-
 function Buyer(fullName, username, address, password) {
 
     this.bidArrays = [];           //For bids on paintings
@@ -11,7 +9,7 @@ function Buyer(fullName, username, address, password) {
 
     Buyer.prototype.printOrders = function() {
         console.log("Orders: ");
-        if(this.orders.length == 0) {
+        if(this.orders.length === 0) {
             console.log("N/A");
         } else
         for(let i = 0; i < this.orders.length; i++) {
@@ -30,7 +28,7 @@ function Buyer(fullName, username, address, password) {
         console.log("Address: " + this.address);
         console.log("Password: " + this.password);
         console.log("Paintings with successful bids: ");
-        if(this.bidArrays.length == 0) {
+        if(this.bidArrays.length === 0) {
             console.log("N/A");
         } else {
             for (let i = 0; i < this.bidArrays.length; i++) {
@@ -57,16 +55,65 @@ function Buyer(fullName, username, address, password) {
         }
     }
 
-
     Buyer.prototype.setBidder = function(bid, painting) {
-        for(let i = 0; i < this.bidArrays.length; i++){
-            if(painting ===this.bidArrays[i].name && this.bidArrays[i].price < bid){
-                this.bidArrays[i].setBidder(bid);
-                console.log("Successfully bid " + bid + " on " + this.bidArrays[i].name);
+
+        for(let i = 0; i < this.bidArrays.length; i++){     //Checks to see if the buyer has a bid and wants to update it
+            if(painting.name === this.bidArrays[i].name && this.bidArrays[i].price < bid){
+                this.bidArrays.splice(i, 1);
+                this.bidArrays.push(painting);
+                console.log("Successfully bid " + bid + " on " + painting.name);
+                return painting.setBidder(bid);
             } else if(painting === this.bidArrays[i].name && this.bidArrays[i].price > bid) {
                 console.log("Sorry, your bid of " + bid + " is under " + this.bidArrays[i].price);
+                return;
             }
         }
+
+        if(bid > painting.highestBid) {
+            this.bidArrays.push(painting);
+            console.log('Successfully bid ' + bid + ' on ' + painting.name);
+            return painting.setBidder(bid);
+        } else {
+            console.log("Sorry, your bid of " + bid + " is under " + painting.highestBid);
+        }
+
+    }
+
+    Buyer.prototype.setPassword = function(password) {
+        this.password = password;
+        console.log("Successfully changed password");
+    }
+
+    Buyer.prototype.getOrderList = function() {
+        return this.orders;
+    }
+
+    Buyer.prototype.getBidList = function() {
+        return this.bidArrays;
+    }
+
+    Buyer.prototype.setAddress = function(address) {
+        this.address = address;
+    }
+
+    Buyer.prototype.getAddress = function() {
+        return this.address;
+    }
+
+    Buyer.prototype.setUsername = function(username) {
+        this.username = username;
+    }
+
+    Buyer.prototype.getUsername = function() {
+        return this.username;
+    }
+
+    Buyer.prototype.getFullname = function(){
+        return this.fullName;
+    }
+
+    Buyer.prototype.setFullname = function(fullName) {
+        this.fullName = fullName;
     }
 
 }
