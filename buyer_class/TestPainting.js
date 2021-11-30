1912c4cdee57bdc0749a0ad369941eb08ad4d070
Angel <90816786+angelDLeon@users.noreply.github.com>
2021-11-29 19:56:49 -0800

    Update TestPainting.js

--- buyer_class/TestPainting.js
+++ buyer_class/TestPainting.js
@@ -1,4 +1,4 @@
-function Painting(name, price, highestBid, artist, dimensions, medium, stock){
+function Painting(name, price, highestBid, artist, dimensions, medium, stock) {
 
     this.namePainting = name;
     this.price = price;
@@ -17,5 +17,15 @@ function Painting(name, price, highestBid, artist, dimensions, medium, stock){
         console.log("Dimensions: " + this.dimensions);
         console.log("Medium: " + this.medium);
     }
-}
+    
+    set setBid(newBid, newHighest) {
+        this.newBid = newBid;
+        this.highestBid = newHighest;        // Painting.setBid = '0000000000'; 
+    }
+    
+    get getBid() {
+        return this.highestBid;          // console.log(Painting.getBid);
+    }
+}  
+  
 module.exports = {Painting};
